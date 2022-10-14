fd5c1e259524b6c4819093ae18b1195555f2c4d7
henrisusanto <henri@member.id>
2022-10-15 05:09:35 +0700

    init

---
+++ services-profile-update.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Services Profile Update
+ *
+ * @package     ServicesProfileUpdate
+ * @author      Henri Susanto
+ * @copyright   2022 Henri Susanto
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Services Profile Update
+ * Plugin URI:  https://github.com/susantohenri
+ * Description: Update user profile base on formidable form submit
+ * Version:     1.0.0
+ * Author:      Henri Susanto
+ * Author URI:  https://github.com/susantohenri
+ * Text Domain: ServicesProfileUpdate
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
