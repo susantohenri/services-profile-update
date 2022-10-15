44f5b68168d876ed5480b8bfe8ebdcb7ac904a90
henrisusanto <henri@member.id>
2022-10-15 07:01:34 +0700

    form & sample csv

--- services-profile-update.php
+++ services-profile-update.php
@@ -19,3 +19,57 @@
  * License:     GPL v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
+
+define('SERVICES_PROFILE_UPDATE_CSV_FILE_SAMPLE', plugin_dir_url(__FILE__) . 'services-profile-update-field-mapping-sample.csv');
+define('SERVICES_PROFILE_UPDATE_CSV_FILE_ACTIVE', plugin_dir_url(__FILE__) . 'services-profile-update-field-mapping-active.csv');
+define('SERVICES_PROFILE_UPDATE_CSV_FILE', plugin_dir_path(__FILE__) . 'services-profile-update-field-mapping-active.csv');
+define('SERVICES_PROFILE_UPDATE_CSV_FILE_SUBMIT', 'services-profile-update-field-mapping-submit');
+
+add_action('admin_menu', function () {
+    add_menu_page('Services Profile Update', 'Services Profile Update', 'administrator', __FILE__, function () {
+
+        if ($_FILES) {
+            if ($_FILES[SERVICES_PROFILE_UPDATE_CSV_FILE_SUBMIT]['tmp_name']) {
+                move_uploaded_file($_FILES[SERVICES_PROFILE_UPDATE_CSV_FILE_SUBMIT]['tmp_name'], SERVICES_PROFILE_UPDATE_CSV_FILE);
+            }
+        }
+?>
+        <div class="wrap">
+            <h1>Services Profile Update</h1>
+            <div id="dashboard-widgets-wrap">
+                <div id="dashboard-widgets" class="metabox-holder">
+                    <div class="">
+                        <div class="meta-box-sortables">
+                            <div id="dashboard_quick_press" class="postbox ">
+                                <div class="postbox-header">
+                                    <h2 class="hndle ui-sortable-handle">
+                                        <span>Field Mapping CSV</span>
+                                        <div>
+                                            <?php if (file_exists(SERVICES_PROFILE_UPDATE_CSV_FILE)) : ?>
+                                                <a class="button button-primary" href="<?= SERVICES_PROFILE_UPDATE_CSV_FILE_ACTIVE ?>" style="text-decoration:none;">Export Current Field Mapping</a>
+                                            <?php endif ?>
+                                            <a class="button button-primary" href="<?= SERVICES_PROFILE_UPDATE_CSV_FILE_SAMPLE ?>" style="text-decoration:none;">Download Empty CSV Sample File</a>
+                                        </div>
+                                    </h2>
+                                </div>
+                                <div class="inside">
+                                    <form name="post" action="" method="post" class="initial-form" enctype="multipart/form-data">
+                                        <div class="input-text-wrap" id="title-wrap">
+                                            <label for="title"> Choose Latest Field Mapping CSV File </label>
+                                            <input type="file" name="<?= SERVICES_PROFILE_UPDATE_CSV_FILE_SUBMIT ?>">
+                                        </div>
+                                        <p>
+                                            <input type="submit" name="save" class="button button-primary" value="Upload Selected CSV">
+                                            <br class="clear">
+                                        </p>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+<?php
+    }, '');
+});
